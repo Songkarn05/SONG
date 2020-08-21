8166869f6e785b5106f46069fc226a7b8820dc66
tonui123 <to.asiatique@gmail.com>
2020-08-21 22:31:47 +0700

    first commit

---
+++ src/components/CONTACT.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CONTACT extends Component {
+    render() {
+        return (
+            <div>
+                <h2>ติดต่อเรา</h2>
+                <p>ลาดพร้าว 71</p>
+            </div>
+        )
+    }
+}
